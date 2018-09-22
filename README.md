1250486a6b7cb41cbf5dff887009074bb7799c46
Jean-Marcel Belmont <jbelmont@foodlogiq.com>
2018-09-22 14:44:55 -0400

    Parsing the lines in csv.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/jbelmont/csv-to-json.svg?branch=master)](https://travis-ci.org/jbelmont/csv-to-json)
+
 # A small library that converts csv files to json
 
 A Rust Library to convert csv to json
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 use std::env;
 use std::fs::File;
-use std::io::prelude::*;
+use std::io;
+use std::io::{BufRead,BufReader,Write,BufWriter};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let file = &args[1];
+    read_file(file);
+}
 
-    let mut f = File::open(file).expect("file not found");
+fn read_file(filename: &str) {
+    let f = File::open(filename).expect("file not found");
 
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
+    let file = BufReader::new(&f);
+    let mut writer = BufWriter::new(io::stdout());
+    for (num, line) in file.lines().enumerate() {
+        if num == 0 {
+            let l = line.unwrap();
+            let v: Vec<&str> = l.split(' ').collect();
+            println!("{:?}", v);
+        }
+    }
 }
