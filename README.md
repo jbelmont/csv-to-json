3f4b69db2a0c8bc732779ab175c9c6b415f88177
Jean-Marcel Belmont <jbelmont@foodlogiq.com>
2018-09-23 19:07:10 -0400

    Finish the cli application that generates json content from csv.

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 [![Build Status](https://travis-ci.org/jbelmont/csv-to-json.svg?branch=master)](https://travis-ci.org/jbelmont/csv-to-json)
 
-# A small library that converts csv files to json
+# An exploration on converting a csv file using minimal library support in Rust
 
-A Rust Library to convert csv to json
+This repository create a json output using a CSV file from [data.gov](https://catalog.data.gov/)
+
+Here is the following csv data for reference:
+
+*demographic_statistics_by_zip_code.csv*:
+
+```csv
+JURISDICTION NAME,COUNT PARTICIPANTS,COUNT FEMALE,PERCENT FEMALE,COUNT MALE,PERCENT MALE,COUNT GENDER UNKNOWN,PERCENT GENDER UNKNOWN
+10001,44,22,0.5,22,0.5,0,0
+10002,35,19,0.54,16,0.46,0,0
+...
+```
+
+## Running the Rust Project
+
+In order to run the the csv-to-json project run the following command:
+
+```bash
+cargo run demographic_statistics_by_zip_code.csv
+
+"[{\"jurisdiction_name\":10001,\"count_participants\":44,\"count_female\":22,\"percent_female\":0.5,\"count_male\":22,\"percent_male\":0.5,\"count_gender_unknown\":0,\"percent_gender_unknown\":0.0},{\"jurisdiction_name\":10002,\"count_participants\":35,\"count_female\":19,\"percent_female\":0.54,\"count_male\":16,\"percent_male\":0.46,\"count_gender_unknown\":0,\"percent_gender_unknown\":0.0},{\"jurisdiction_name\":10003,\"count_participants\":1,\"count_female\":1,\"percent_female\":1.0,\"count_male\":0,\"percent_male\":0.0,\"count_gender_unknown\":0,\"percent_gender_unknown\":0.0},{\"jurisdiction_name\":10004,\"count_participants\":0,\"count_female\":0,\"percent_female\":0.0,\"count_male\":0,\"percent_male\":0.0,\"count_gender_unknown\":0,\"percent_gender_unknown\":0.0},{\"jurisdiction_name\":10005,\"count_participants\":2,\"count_female\":2,\"percent_female\":1.0,\"count_male\":0,\"percent_male\":0.0,\"count_gender_unknown\":0,\"percent_gender_unknown\":0.0},{\"jurisdiction_name\":10006,\"count_participants\":6,\"count_female\":2,\"percent_female\":0.33,\"count_male\":4,\"percent_male\":0.67,\"count_gender_unknown\":0,\"percent_gender_unknown\":0.0},{\"jurisdiction_name\":10007,\"count_participants\":1,\"count_female\":0,\"percent_female\":0.0,\"count_male\":1,\"percent_male\":1.0,\"count_gender_unknown\":0,\"percent_gender_unknown\":0.0},{\"jurisdiction_name\":10009,\"count_participants\":2,\"count_female\":0,\"percent_female\":0.0,\"count_male\":2,\"percent_male\":1.0,\"count_gender_unknown\":0,\"percent_gender_unknown\":0.0},
+...
+```
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+extern crate serde;
+extern crate serde_json;
+
+#[macro_use]
+extern crate serde_derive;
+
 use std::env;
 use std::fs::File;
-use std::io;
-use std::io::{BufRead,BufReader,Write,BufWriter};
+use std::io::{BufReader,BufRead};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,16 +15,69 @@ fn main() {
     read_file(file);
 }
 
-fn read_file(filename: &str) {
-    let f = File::open(filename).expect("file not found");
+#[derive(Default, Serialize, Deserialize, Debug, Copy, Clone)]
+struct Demographic {
+    jurisdiction_name: usize,
+    count_participants: usize,
+    count_female: usize,
+    percent_female: f64,
+    count_male: usize,
+    percent_male: f64,
+    count_gender_unknown: usize,
+    percent_gender_unknown: f64,
+}
 
+impl Demographic {
+    fn new() -> Self {
+        Default::default()
+    }
+}
+
+fn read_file(filename: &str) {
+    let f = File::open(filename).unwrap();
     let file = BufReader::new(&f);
-    let mut writer = BufWriter::new(io::stdout());
+    let mut demographic: Demographic = Demographic::new();
+    let mut keys = vec![String::from("")];
+    let mut list: Vec<Demographic> = vec![];
     for (num, line) in file.lines().enumerate() {
         if num == 0 {
             let l = line.unwrap();
-            let v: Vec<&str> = l.split(' ').collect();
-            println!("{:?}", v);
+            let v = l.split(',')
+                .map(|field| field.to_lowercase().replace(" ", "_"))
+                .collect();
+            keys = v;
+        } else {
+            let l = line.unwrap();
+            let values: Vec<String> = l.split(',')
+                .map(|val| val.to_string())
+                .collect();
+
+            for item in keys.iter() {
+                if item == "jurisdiction_name" {
+                    demographic.jurisdiction_name = values[0].parse().unwrap();
+                } else if item == "count_participants" {
+                    demographic.count_participants = values[1].parse().unwrap();
+                } else if item == "count_female" {
+                    demographic.count_female = values[2].parse().unwrap();
+                } else if item == "percent_female" {
+                    demographic.percent_female = values[3].parse().unwrap();
+                } else if item == "count_male" {
+                    demographic.count_male = values[4].parse().unwrap();
+                } else if item == "percent_male" {
+                    demographic.percent_male = values[5].parse().unwrap();
+                } else if item == "count_gender_unknown" {
+                    demographic.count_gender_unknown = values[6].parse().unwrap();
+                } else if item == "percent_gender_unknown" {
+                    demographic.percent_gender_unknown = values[7].parse().unwrap();
+                }
+            }
+            list.push(demographic);
         }
     }
+
+    // Serialize it to a JSON string.
+    let j = serde_json::to_string(&list).expect("could not serialize demographics");
+
+    // Print, write to a file, or send to an HTTP server.
+    println!("{:?}", j);
 }
